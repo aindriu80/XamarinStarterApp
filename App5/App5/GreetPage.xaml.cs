ce05cd212782a84a52ab3087a6b09f5e3fdb82ee
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-05-22 11:28:58 +0100

    Property Element Syntax

--- App5/App5/GreetPage.xaml.cs
+++ App5/App5/GreetPage.xaml.cs
@@ -12,12 +12,19 @@ namespace App5
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class GreetPage : ContentPage
 	{
-		public GreetPage ()
-		{
-			InitializeComponent ();
-		    slider.Value = 0.5;
-		}
+	    public GreetPage()
+	    {
+	        InitializeComponent();
+	        slider.Value = 0.5;
 
-	    
+	        var x = new OnPlatform<Thickness>
+	        {
+                Android = new Thickness(),
+                iOS = new Thickness(0,20,0,0)
+	        };
+
+	    }
+
+
 	}
 }
