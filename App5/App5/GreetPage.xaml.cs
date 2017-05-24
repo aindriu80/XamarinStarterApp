b74510b09e55d5c812e64ce98ff58f21c13d61bc
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-05-24 11:49:10 +0100

    Relative Layout in C#

--- App5/App5/GreetPage.xaml.cs
+++ App5/App5/GreetPage.xaml.cs
@@ -19,8 +19,12 @@ namespace App5
 
             var x = new OnPlatform<Thickness>
             {
+#pragma warning disable 612
                 Android = new Thickness(),
+#pragma warning restore 612
+#pragma warning disable 612
                 iOS = new Thickness(0,20,0,0)
+#pragma warning restore 612
             };
 
         }
