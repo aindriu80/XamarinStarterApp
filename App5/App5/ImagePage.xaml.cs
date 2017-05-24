720f75af83c02bf21c6fc2e7992ce1b5e069cb6a
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-05-24 17:20:57 +0100

    Embedded Image (Android) C#

--- App5/App5/ImagePage.xaml.cs
+++ App5/App5/ImagePage.xaml.cs
@@ -15,9 +15,10 @@ namespace App5
 		public ImagePage ()
 		{
 			InitializeComponent ();
-		    var imageSource= new UriImageSource {Uri = new Uri("http://lorempixel.com/1920/1080/sports/7/") };
-		    imageSource.CachingEnabled = false;
-		    image.Source = imageSource;
+		    //var imageSource= new UriImageSource {Uri = new Uri("http://lorempixel.com/1920/1080/sports/7/") };
+		    //imageSource.CachingEnabled = false;
+		    //image.Source = imageSource;
+            image.Source = ImageSource.FromResource("App5.Droid.Images.background.jpg");
 
 		}
 	}
