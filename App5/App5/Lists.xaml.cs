672fee0935466e9eef577d79e020699425697562
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-05-27 10:29:55 +0100

    Context Actions - View Delete

--- App5/App5/Lists.xaml.cs
+++ App5/App5/Lists.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,25 +13,20 @@ namespace App5
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Lists : ContentPage
 	{
+	    private ObservableCollection<Contact> _contacts;
 		public Lists ()
 		{
 			InitializeComponent ();
 
 
 
-		    listView.ItemsSource = new List<ContactGroup>
-		    {
-                new ContactGroup("A", "A")
-                {
+		    _contacts = new ObservableCollection<Contact>
+            {
                     new Contact {Name = "Aindriu", ImageUrl = "http://lorempixel.com/100/100/people/1"},
-                },
-                new ContactGroup("J","J")
-                {
                     new Contact {Name = "John", ImageUrl = "http://lorempixel.com/100/100/people/2", Status = "Hey, let's talk!"}
-                }
-		        
-
 		    };
+
+            listView.ItemsSource = _contacts;
 		}
 
 	    private void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
@@ -43,6 +39,21 @@ namespace App5
 	    {
 	       var contact = e.SelectedItem as Contact;
 	        DisplayAlert("Selected", contact.Name, "OK");
+	    }
+
+	    private void Call_Clicked(object sender, EventArgs e)
+	    {
+	        var menuItem = sender as MenuItem;
+	        var contact = menuItem.CommandParameter as Contact;
+
+	        DisplayAlert("Call", contact.Name, "OK");
+
+	    }
+
+	    private void Delete_Clicked(object sender, EventArgs e)
+	    {
+	        var contact = (sender as MenuItem).CommandParameter as Contact;
+	        _contacts.Remove(contact);
 	    }
 	}
 }
