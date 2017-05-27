e476c30d0229b11d9c63b86809d19b412241548f
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-05-27 09:40:04 +0100

    Render A-Z Index (iOS) right side. A-Z title header

--- App5/App5/Lists.xaml.cs
+++ App5/App5/Lists.xaml.cs
@@ -18,10 +18,18 @@ namespace App5
 
 
 
-		    listView.ItemsSource = new List<Contact>
+		    listView.ItemsSource = new List<ContactGroup>
 		    {
-		        new Contact {Name = "Aindriu", ImageUrl = "http://lorempixel.com/100/100/people/1"},
-		        new Contact {Name = "John", ImageUrl = "http://lorempixel.com/100/100/people/2", Status = "Hey, let's talk!"}
+                new ContactGroup("M", "M")
+                {
+                    new Contact {Name = "Aindriu", ImageUrl = "http://lorempixel.com/100/100/people/1"},
+                },
+                new ContactGroup("J","J")
+                {
+                    new Contact {Name = "John", ImageUrl = "http://lorempixel.com/100/100/people/2", Status = "Hey, let's talk!"}
+                }
+		        
+
 		    };
 		}
 	}
