4f9fcaecc965c41544ab4b22c09cf1f6c2bd00fc
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-05-30 10:14:50 +0100

    Picker + retrieving from hardcoded list of items

---
+++ App5/App5/ContactMethod.cs
@@ -0,0 +1,9 @@
+namespace App5
+{
+    public class ContactMethod
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
--- App5/App5/MainPage5.xaml.cs
+++ App5/App5/MainPage5.xaml.cs
@@ -9,13 +9,30 @@ using Xamarin.Forms.Xaml;
 
 namespace App5
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MainPage5 : ContentPage
 	{
+	    private IList<ContactMethod> _contactMethods;
 		public MainPage5 ()
 		{
 			InitializeComponent ();
+
+		    _contactMethods = GetContactMethods();
+
+		    foreach (var method in _contactMethods)
+		    {
+		        ContactMethods.Items.Add(method.Name);
+		    }
 		}
+
+	    private IList<ContactMethod> GetContactMethods()
+	    {
+	        return new List<ContactMethod>
+	        {
+                new ContactMethod{ Id = 1, Name ="SMS"},
+                new ContactMethod{ Id = 2, Name ="Email"}
+	        };
+	    }
 
 	    private void Switch_OnToggled(object sender, ToggledEventArgs e)
 	    {
@@ -35,6 +52,14 @@ namespace App5
 	    private void Entry_OnTextChanged(object sender, TextChangedEventArgs e)
 	    {
 	        PasswordLabel2.Text = e.NewTextValue;
+	    }
+
+	    private void Picker_OnSelectedIndexChanged(object sender, EventArgs e)
+	    {
+	       var name = ContactMethods.Items[ContactMethods.SelectedIndex];
+	       var contactMethod = _contactMethods.Single(cm => cm.Name == name);
+
+            DisplayAlert("Selection",name, "OK");
 	    }
 	}
 }
